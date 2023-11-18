7dccdd803e9687728cfa4da71d87d652efa831da
Anderson Fernandes <cabula21@yahoo.com.br>
2023-11-18 13:21:18 -0300

    WORKSHOP - Landingpage

---
+++ script/script.js
@@ -0,0 +1,7 @@
+function gerarLink() {
+    // Substitua esta linha pelo código que gera o link desejado
+    var linkGerado = "https://youtu.be/4dQtz1PpY9A?si=M8629Vc3wEuks4V1";
+
+    // Redirecionar para o link gerado
+    window.location.href = linkGerado;
+  }
